30fe18330fbb38c31e1806daec8a392126aabca6
Andreas Eckerle <a.eckerle@iroka.de>
2021-09-05 10:45:52 +0200

    first complete version passing all tests

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -15,6 +15,8 @@ cur = conn.cursor()
 
 cur.execute(sql_create_table_accounts)
 
+number = ""
+
 def select_account(conn, number):
     """
         Query card by account
@@ -46,6 +48,16 @@ def insert_card(conn, number, pin):
     result = cur.lastrowid
     print(result)
     return result
+
+def add_money(conn, number, amount):
+    print(number)
+    row = select_account(conn, number)
+    amount = row[3] + amount
+    cur = conn.cursor()
+    cur.execute("UPDATE card SET balance = ? WHERE number = ?", (amount, number))
+
+    conn.commit()
+    return
 
 
 def get_luhn_crc(card_number):
@@ -92,11 +104,44 @@ def create_account():
     print(number)
     print("Your card PIN:")
     print("{0:04d}".format(pin))
+    return number
+
+def add_income(number):
+    print("Enter income:")
+    income = int(input())
+    add_money(conn, number, income)
+    return
+
+def do_transfer(number):
+    print("Transfer")
+    print("Enter card number:")
+    to_number = input()
+    print(to_number[0:15])
+    if len(to_number) != 16:
+        print("Probably you made a mistake in the card number. Please try again!")
+        return
+    if get_luhn_crc(to_number[0:15]) != int(to_number[15]):
+        print("Probably you made a mistake in the card number. Please try again!")
+        return
+    print(get_luhn_crc(to_number[0:15]))
+    print(to_number[0:15])
+    if not select_account(conn, to_number):
+        print("Such a card does not exist.")
+        return
+    print("Enter how much money you want to transfer:")
+    to_money = int(input())
+    row = select_account(conn, number)
+    if row[3] < to_money:
+        print("Not enough money!")
+        return
+    add_money(conn, number, -(to_money))
+    add_money(conn, to_number, to_money)
+    print("Success!")
     return
 
 def login():
     print("Enter your card number:")
-    card_number = input()
+    number = input()
     print("Enter your PIN:")
     pin = input()
     #if len(card_number) == 16:
@@ -106,37 +151,59 @@ def login():
     #    if card_iin == IIN and account in cards:
     #        account_data = cards[account]
     #        if account_data[1] == pin and account_data[0] == card_crc:
-    result = select_account(conn, card_number)
+    result = select_account(conn, number)
     if result and result[2] == pin:
         print("You have successfully logged in!")
-        return True
+        return number
     print("Wrong card number or PIN!")
-    return False
+    return None
 
 def login_menu():
     while True:
         print("1. Balance")
-        print("2. Log out")
+        print("2. Add income")
+        print("3. Do transfer")
+        print("4. Close account")
+        print("5. Log out")
         print("0. Exit")
         mode = input()
-        if mode in "012":
+        if mode in "012345":
             return mode
+
+def close_account(number):
+    cur = conn.cursor()
+    cur.execute("DELETE FROM card WHERE number = ?", (number,))
+
+    conn.commit()
+    return
+
+def login_loop(number):
+    while True:
+        login_mode = login_menu()
+        if login_mode == "0":
+            return login_mode
+        elif login_mode == "5":
+            return ""
+        elif login_mode == "2":
+            add_income(number)
+        elif login_mode == "3":
+            do_transfer(number)
+        elif login_mode == "4":
+            close_account(number)
+            print("The account has been closed!")
+        else:
+            row = select_account(conn, number)
+            print("Balance: {}".format(row[3]))
 
 while True:
     mode = show_menu()
     if mode == "1":
-        create_account()
+        number = create_account()
     elif mode == "2":
         log_in = login()
         if log_in:
-            login_mode = login_menu()
-            if login_mode == "0":
-                mode = "0"
-            elif login_mode == "2":
-                pass
-            else:
-                print("Balance: 0")
-
+            number = log_in
+            mode = login_loop(number)
     if mode == "0":
         break
 
